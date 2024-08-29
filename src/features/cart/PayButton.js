f98cbfcb84defe74666e6e7ca36544ce7890e51d
Chetna0524 <chetnagupta2021@gmail.com>
2024-08-29 15:47:29 +0530

    changes in paybutton

--- src/features/cart/PayButton.js
+++ src/features/cart/PayButton.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import { Spinner } from "react-bootstrap";
 
 const PayButton = ({ cartItems }) => {
 	const { currentUser } = useSelector((state) => state.auth);
+	const [isLoading, setLoading] = useState("idle");
+	console.log("cartItems paybtn", cartItems);
+
 	const handleCheckout = async () => {
 		try {
+			setLoading("loading");
 			const res = await axios.post(
 				"https://stripe-backend-omej.onrender.com/create-checkout-session",
 				{
@@ -13,6 +18,7 @@ const PayButton = ({ cartItems }) => {
 					/* customer_email: currentUser?.email, */
 				}
 			);
+
 			if (res?.data?.url) {
 				console.log(res.data.url);
 				window.location.href = res.data.url;
@@ -22,9 +28,21 @@ const PayButton = ({ cartItems }) => {
 		}
 	};
 
+	console.log("loading", isLoading);
+
 	return (
 		<>
 			<button className="btn-comm" type="submit" onClick={handleCheckout}>
+				{isLoading === "loading" ? (
+					<Spinner
+						as="span"
+						animation="border"
+						size="sm"
+						role="status"
+						aria-hidden="true"
+						style={{ display: "inline-block", marginRight: "5px" }}
+					/>
+				) : null}
 				Checkout
 			</button>
 		</>
